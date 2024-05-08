ca6acb8413f51cf695946c47f058689c499cfcd7
Dnyandeep <dnyandeepc18@gmail.com>
2024-05-08 21:00:08 +0530

    flask app

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
+
 
 @dataclass
 class DataIngestionConfig:
@@ -57,6 +59,10 @@ if __name__ == "__main__":
 
     data_transformation = DataTransformation()
 
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
 
+    model_trainer = ModelTrainer()
 
+    r2 = model_trainer.initiate_model_trainer(train_arr, test_arr)
+
+    print("r2 score of best model:", r2)
